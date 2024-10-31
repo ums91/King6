844941fad8902525739779b426a08dccce588658
Omer <136714975+ums91@users.noreply.github.com>
2024-10-31 23:33:09 +0530

    Update kng6.py

--- kng6.py
+++ kng6.py
@@ -14,7 +14,7 @@ def create_coauthored_commit(repo_name):
         repo = g.get_repo(repo_name)
         commit_message = (
             "Automated commit for Pair Extraordinaire achievement\n\n"
-            "Co-authored-by: ums91 <omargoroo91@gmail.com>\n"
+            "Co-authored-by: ensiferum930 <ensiferum930@gmail.com>\n"
             "Co-authored-by: learnwithums <learnwithums@gmail.com>"
         )
         repo.create_file(
